00d5863f7ec0cf577f1b1b65cecebeca6596a0a2
JerryYin <jerryyin@JerryYins-MacBook-Pro.local>
2017-11-09 17:27:49 +0800

    init commit

---
+++ src/com/jerryyin/JavaMailApplication.java
@@ -0,0 +1,10 @@
+package com.jerryyin;
+
+public class JavaMailApplication {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.print("---------JavaMailApp is runninng...-----------");
+    }
+}
